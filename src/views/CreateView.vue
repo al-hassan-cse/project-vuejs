f9fab5d3018804f11e6fb68856a7f5a8ac63f10f
alhassan <alhassan.cse@gmail.com>
2023-09-01 21:21:26 +0600

    first commit

---
+++ src/views/CreateView.vue
@@ -0,0 +1,52 @@
+<template>
+    <div class="container">
+        <div class="about">
+            <h1>{{ title }}</h1>
+            <div class="row">
+                <div class="col-md-12">
+                    <form v-on:submit.prevent="doCreate" method="POST" action="create.php">
+                        <div class="form-group">
+                            <input type="text" class="form-control" id="name" placeholder="name">
+                        </div>
+                        <div class="form-group">
+                            <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
+                        </div>
+                        <div class="form-group">
+                            <input type="password" class="form-control" id="password" placeholder="Password">
+                        </div>
+                        <div class="form-group">
+                            <textarea class="form-control" id="Comments" placeholder="Comments" rows="3"></textarea>
+                        </div>
+                        <div class="form-group">
+                            <select class="form-control" id="exampleFormControlSelect1">
+                                <option>Active</option>
+                                <option>InActive</option>
+                            </select>
+                        </div>
+                        <div class="form-group">
+                            <button type="submit" class="btn btn-info">Submit</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+    export default {
+        data(){
+            return{
+                title:'Create user list'
+            }
+        },
+        methods: { 
+			doCreate: function () {
+				alert(111);
+			}
+		}
+    }
+</script>
+
+
+
+ 
---
+++ src/views/HomeView.vue
@@ -0,0 +1,20 @@
+<link rel="stylesheet" type="text/css" href="css/bootstrap.css" />
+
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'HomeView',
+  components: {
+    HelloWorld
+  }
+}
+</script>
